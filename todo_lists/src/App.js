4442c363d3f2a462760f53261bd96d7edce31b95
anushkaengavle <anushkaengavle@gmail.com>
2022-11-13 23:32:15 +0530

    company website temp

---
+++ todo_lists/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';  
+class App extends React.Component {  
+   render() {     
+      return (  
+          <div>  
+            <h1> Welcome to { this.props.name } </h1>    
+            <p> <h4> Javatpoint is one of the best Java training institute in Noida, Delhi, Gurugram, Ghaziabad and Faridabad. </h4> </p>          
+          </div>  
+      );  
+   }  
+}  
+export default App;  
